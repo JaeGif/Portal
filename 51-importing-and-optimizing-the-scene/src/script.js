66c2161e11533611d8737a3daddb45d5c63a382c
Jae <giffordjacob0@gmail.com>
2024-06-21 16:38:19 -0700

    add sky background for bonus

--- 51-importing-and-optimizing-the-scene/src/script.js
+++ 51-importing-and-optimizing-the-scene/src/script.js
@@ -9,6 +9,7 @@ import firefliesFragmentShader from './shaders/fireflies/fragment.glsl';
 import portalVertexShader from './shaders/portal/vertex.glsl';
 import portalFragmentShader from './shaders/portal/fragment.glsl';
 
+import { Sky } from 'three/addons/objects/Sky.js';
 /**
  * Base
  */
@@ -75,6 +76,7 @@ gltfLoader.load('baked-portal.glb', (gltf) => {
   portalLightMesh.material = portalLightMaterial;
   bakedMesh.material = sceneMaterial;
 
+  gltf.scene.position.y = -0.75;
   scene.add(gltf.scene);
 });
 
@@ -192,6 +194,52 @@ if (gui) {
     .step(1)
     .name('firefliesSize');
 }
+
+// Add Sky
+const sky = new Sky();
+sky.scale.setScalar(450000);
+scene.add(sky);
+
+const sun = new THREE.Vector3();
+
+const skyParameters = {
+  turbidity: 10,
+  rayleigh: 3,
+  mieCoefficient: 0.1,
+  mieDirectionalG: 0.95,
+  elevation: -2.15,
+  azimuth: -167.2,
+  exposure: renderer.toneMappingExposure,
+};
+
+function updateSky() {
+  const uniforms = sky.material.uniforms;
+  uniforms['turbidity'].value = skyParameters.turbidity;
+  uniforms['rayleigh'].value = skyParameters.rayleigh;
+  uniforms['mieCoefficient'].value = skyParameters.mieCoefficient;
+  uniforms['mieDirectionalG'].value = skyParameters.mieDirectionalG;
+
+  const phi = THREE.MathUtils.degToRad(90 - skyParameters.elevation);
+  const theta = THREE.MathUtils.degToRad(skyParameters.azimuth);
+
+  sun.setFromSphericalCoords(1, phi, theta);
+
+  uniforms['sunPosition'].value.copy(sun);
+
+  renderer.toneMappingExposure = skyParameters.exposure;
+  renderer.render(scene, camera);
+}
+if (gui) {
+  gui.add(skyParameters, 'turbidity', 0.0, 20.0, 0.1).onChange(updateSky);
+  gui.add(skyParameters, 'rayleigh', 0.0, 4, 0.001).onChange(updateSky);
+  gui.add(skyParameters, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(updateSky);
+  gui.add(skyParameters, 'mieDirectionalG', 0.0, 1, 0.001).onChange(updateSky);
+  gui.add(skyParameters, 'elevation', -5, 10, 0.01).onChange(updateSky);
+  gui.add(skyParameters, 'azimuth', -180, 180, 0.1).onChange(updateSky);
+  gui.add(skyParameters, 'exposure', 0, 1, 0.0001).onChange(updateSky);
+}
+updateSky();
+
 /**
  * Animate
  */
